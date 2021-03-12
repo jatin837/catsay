7a70adcdfede98d4a39ce3f72a9b750b80267a06
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-03-13 02:11:24 +0530

    add structopt dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+extern crate structopt;
+
+use structopt::StructOpt;
+#[derive(StructOpt)]
+struct Options {
+    message: String // [1]
+}
 fn main() {
-    let message = std::env::args().nth(1)
-        .expect("Missing the message. Usage: catsay <message>");
+    let options = Options::from_args(); // [2]
+    let message = options.message;
     println!("{}", message);
     println!(" \\");
     println!("  \\");
